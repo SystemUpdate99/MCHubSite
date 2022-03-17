53566814850e546583fce971664d65ab1e3141a4
SystemUpdate99 <systemupdate55@gmail.com>
2022-03-17 17:42:39 +0000

    new url open

--- src/index.js
+++
@@ -1,9 +0,0 @@
-let mouseMoved = false;
-console.log("hi");
-//window.open("steam://rungameid/386940","self");
-document.onmousedown = function() {
-    if(!mouseMoved){
-        window.open("steam://rungameid/386940");
-        mouseMoved = true;
-    }
-}
